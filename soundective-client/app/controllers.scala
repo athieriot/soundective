20c76a48decbcd05bc45fe08225b976d67a7c40f
Aurélien Thieriot <a.thieriot@gmail.com>
2011-03-10 22:00:35 +0100

    some unfinished work

--- soundective-client/app/controllers.scala
+++ soundective-client/app/controllers.scala
@@ -3,8 +3,15 @@ package controllers
 import play._
 import play.mvc._
 
+import models.Song
+
 object Application extends Controller {
     
     def index = Template
-    
+
+    def add(name: String) = {
+      val song = new Song(name)
+      Logger.info(Song.count.toString)
+      song.save
+    }
 }
